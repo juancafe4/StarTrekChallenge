e4af1d04b30a615d5c7159fb173d1d001bce1aae
Juan Carlos <juancafe2@gmail.com>
2016-12-08 12:35:29 -0800

    Setting up the server with webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    './client/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  target: 'web',
+  eslint: {
+  configFile: './.eslintrc'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+    module: {
+    // preLoaders: [
+   //    {
+   //      test: /\.jsx?$/,
+   //      exclude: /node_modules/,
+   //      loader: 'eslint-loader'
+   //    },
+  //  ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+    ]
+  }
